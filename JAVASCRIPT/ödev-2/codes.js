6b6b2cf5795d24b13da5b551fc3a0ba7b2c7266f
MUKAN AYAZOĞLU <postaturkey@yaani.com>
2025-11-06 18:48:19 +0300

    ödev-2 is updated, ödev-3 is uploaded.

--- JAVASCRIPT/ödev-2/codes.js
+++ JAVASCRIPT/ödev-2/codes.js
@@ -1,36 +1,21 @@
 
 
+let theNumber=1;
+
+
 let theClick= document.querySelector("#click");
 let theText= document.querySelector("#text");
 let theToast= document.querySelector("#toast");
 let listGroup=document.querySelector("#list");
+let theCloseBtn=document.querySelector(`close${theNumber}`);
 
 
-function addLi(className, text) {
 
-        let createdLi=document.createElement("li");
-        createdLi.classList.add(className);
-        createdLi.innerHTML=
-        `
-        <input class="form-check-input me-1" type="checkbox" value="" id="firstCheckbox">
-        <label class="form-check-label" for="firstCheckbox">
-        ${text}
-         </label>
-         <button type="button" class="btn-close" data-bs-dismiss=${className} aria-label="Close" style="float: right"></button>
-
-         
-         
-    `;
-
-    listGroup.appendChild(createdLi);
-
-
-}
 
 function toast(message){
 
     return `
-<div class="toast show top-0 end-0 position-absolute" role="alert" aria-live="assertive" aria-atomic="true" style="float: right; position: static">
+<div class="toast show top-0 end-0 position-absolute" data-bs-delay="4000" role="alert" aria-live="assertive" aria-atomic="true" style="float: right; position: static">
   <div class="d-flex">
     <div class="toast-body">
       ${message}
@@ -40,6 +25,41 @@ function toast(message){
     </div>
 
     `
+
+}
+
+
+function addLi(className, text, theNumber) {
+
+        let createdLi=document.createElement("li");
+        createdLi.classList.add(className);
+
+        createdLi.innerHTML=
+        `
+        <input class="form-check-input me-1" type="checkbox" value="" id="firstCheckbox">
+        <label class="form-check-label" for="firstCheckbox">
+        ${text}
+         </label>
+         <button type="button" id="close${theNumber}" class="btn-close" data-bs-dismiss="li${theNumber}" aria-label="Close" style="float: right"></button>
+
+    `;
+
+        localStorage.setItem(theNumber.toString(),text);
+
+
+    listGroup.appendChild(createdLi);
+
+    theNumber++;
+
+
+}
+
+
+function removeLi() {
+    theNumber--;
+
+    let theLi=document.querySelector(`#li${theCloseBtn.id.valueOf().slice(5,theCloseBtn.id.length)}`);
+    theLi.remove();
 
 }
 
@@ -64,4 +84,6 @@ theClick.addEventListener("click", function(){
 
 
 
-})
+});
+
+theCloseBtn.addEventListener("click", removeLi());

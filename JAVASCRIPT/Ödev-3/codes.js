b3ebf17ea63e12b1cb77ba67c479dbd2f6311d9c
MUKAN AYAZOĞLU <postaturkey@yaani.com>
2025-11-11 19:46:24 +0300

    ödev-3 folder is updated.

---
+++ JAVASCRIPT/Ödev-3/codes.js
@@ -0,0 +1,83 @@
+let all= document.querySelector("#btnAll");
+let korea= document.querySelector("#btnKorea");
+let japan= document.querySelector("#btnKJapan");
+let china= document.querySelector("#btnChina");
+
+console.log(all.value);
+
+
+
+
+let objKorea = { all: {
+    1.:"https://twoplaidaprons.com/wp-content/uploads/2020/09/tteokbokki-top-down-view-of-tteokbokki-in-a-bowl-500x500.jpg",
+        title: "Tteokbokki",
+        price: 10.99,
+        recipe: "Spicy rice cakes, serving with fish cake"
+    },
+    2.: {
+        img:"https://dwellbymichelle.com/wp-content/uploads/2020/05/DWELL-bibimbap.jpg",
+        title: "Bibimbap",
+        price: 8.99,
+        recipe: "Boiling vegetables, serving with special hot sauce "
+    },
+    3.: {
+        img:"https://www.curiouscuisiniere.com/wp-content/uploads/2020/04/Jajangmyeon-Korean-Noodles-in-Black-Bean-Sauce5.1200H-720x540.jpg",
+        title: "Jajangmyeon",
+        price: 15.99,
+        recipe: "Black bean sauce noodle, serving with green onion"
+    }
+}
+
+let objJapan = { all: {
+        1.:"https://www.forkknifeswoon.com/wp-content/uploads/2014/10/simple-homemade-chicken-ramen-fork-knife-swoon-01.jpg",
+        title: "Chicken Ramen",
+        price: 7.99,
+        recipe: "Chicken noodle soup, serving with vegetables such as soy bean, green onion. In an optional you can ask for egg"
+    },
+    2.: {
+        img:"https://www.manusmenu.com/wp-content/uploads/2017/08/Onigiri-3-1-of-1.jpg",
+        title: "Onigiri",
+        price: 9.99,
+        recipe: "Rice Sandwich, serving with soy sauce"
+    },
+    3.: {
+        img:"https://www.justonecookbook.com/wp-content/uploads/2011/10/Dorayaki-New-500x400.jpg",
+        title: "Doroyaki",
+        price: 9.99,
+        recipe: "Red bean paste dessert, serving with honey"
+    }
+}
+
+let objChina = { all: {
+        1.:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeNPSBM0B3GNx5RqHMnLSYE28La7hAZwhDig&s",
+        title: "Dan Dan Mian",
+        price: 5.99,
+        recipe: "Dan dan noodle, serving with green onion"
+    },
+    2.: {
+        img:"https://salu-salo.com/wp-content/uploads/2013/02/Yangzhou-Fried-Rice1.jpg",
+        title: "Yangzhou Fried Rice",
+        price: 12.99,
+        recipe: "Yangzhou style fried rice, serving with bean and pickles "
+    },
+    3.: {
+        img:"https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/F688C2F6-86EC-46C4-B9C7-A6BA01DF7437/Derivates/32E3E72A-F786-406D-AF7F-B30980A9AC6C.jpg",
+        title: "Ma Yi Shang Shu",
+        price: 12.99,
+        recipe: "Hot pepper sauce noodle, serving with soy bean and onion"
+    }
+}
+
+let objAll= {...objJapan,...objKorea,...objChina};
+console.log(objAll);
+
+
+
+korea.addEventListener("click", function(){
+
+
+
+
+
+
+})
